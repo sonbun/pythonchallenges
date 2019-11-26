8885c4dbf07962f6df85fa3125010dcbca5f0606
sonbun <49052848+sonbun@users.noreply.github.com>
2019-11-26 08:36:53 +0800

    Add files via upload

---
+++ GC/GC05.py
@@ -0,0 +1,20 @@
+#Sequence - a set of instructions
+x = 0
+x = x + 9
+answer = x**2
+print(answer)
+
+#Selection - if statements, using conditions (true/false)
+if x == 9:
+    print("yay 9")
+else:
+    print("Nay")
+
+#Repetition - repeating a set of instruction
+for y in range(2):
+    print(y)
+
+#Variable
+total = 0
+for x in range(24):
+    total = total + x
